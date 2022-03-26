b68479ffd30842d07a3ff4148bed279d407c7e30
Gaston Caminiti <gastoncaminiti@gmail.com>
2022-03-26 10:37:07 -0300

    Enemy AI[Part I] and Weapons
    Provoked Behavior
    EngageTarget Method
    ChaseTarget   Method
    AttackTarget  Method

--- EJEMPLO - FPS/Assets/Scripts/EnemyAI.cs
+++ EJEMPLO - FPS/Assets/Scripts/EnemyAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,24 +6,56 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    [SerializeField] Transform target;
-    [SerializeField] float chaseRange = 5f;
+    [SerializeField] private Transform target;
+    [SerializeField] private float chaseRange = 5f;
 
-    NavMeshAgent navMeshAgent;
-    float distanceToTarget = Mathf.Infinity;
+    private NavMeshAgent navMeshAgent;
+    private float distanceToTarget = Mathf.Infinity;
+
+    private bool isProvoked = false;
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+
     }
 
     void Update()
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if (distanceToTarget <= chaseRange)
+
+        if (isProvoked)
         {
-            navMeshAgent.SetDestination(target.position);
+            EngageTarget();
         }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log("ENEMY ATTACK "+name);
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
     }
 
     private void OnDrawGizmosSelected()
